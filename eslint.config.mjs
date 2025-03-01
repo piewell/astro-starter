b6ccc605f9baff91346b5797a50d5293dd945e62
piewell <jenewardwell@gmail.com>
2025-03-01 15:24:05 -0800

    [C] Update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import js from "@eslint/js";
+
+export default [
+  {
+    ignores: [".astro/**"]
+  },
+  js.configs.recommended,
+  // Add modifications to rules here, such as:
+  {
+    rules: {
+      // semi: ["warn", "always"]
+    }
+  },
+  // add more generic rule sets above
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    }
+  }
+];
